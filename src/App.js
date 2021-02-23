31a79cf041af5f9512f984c7f02a42214572d655
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-23 21:52:25 +0400

    initialize css and setup chatengine

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import 'App.css';
 const App = () => {
-	return <div>hello</div>;
+	return <ChatEngine height="100vh" projectID="5d530162-b84b-4b81-b000-aef317bee353" userName="" userSecret="" />;
 };
 
 export default App;
