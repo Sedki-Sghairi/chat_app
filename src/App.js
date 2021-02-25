6e16453446c953fc1cc370cd1657af09accf6af9
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-25 22:35:19 +0400

    add login and customize chatFeed component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			projectID="5d530162-b84b-4b81-b000-aef317bee353"
 			userName={process.env.REACT_APP_ROOT}
 			userSecret={process.env.REACT_APP_SECRET}
+			renderChatFeed={(props) => <ChatFeed {...props} />}
 		/>
 	);
 };
